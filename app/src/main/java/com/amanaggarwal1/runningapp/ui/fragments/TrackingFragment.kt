03203cc3aeddb36e9029226c91f6456dcdd485b6
amanaggarwal1 <amanaggarwal2013@gmail.com>
2020-11-09 15:54:23 +0530

    Start Location service setup on start button

--- app/src/main/java/com/amanaggarwal1/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/amanaggarwal1/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,30 +1,42 @@
 package com.amanaggarwal1.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.amanaggarwal1.runningapp.R
+import com.amanaggarwal1.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.amanaggarwal1.runningapp.services.TrackingService
 import com.amanaggarwal1.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
+import timber.log.Timber
 
 @AndroidEntryPoint
-class TrackingFragment: Fragment(R.layout.fragment_tracking)  {
+class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
-    private var googleMap : GoogleMap? = null
+    private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
-
-        mapView.getMapAsync{
-            googleMap = it
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+        mapView.getMapAsync {
+            map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+            Intent(requireContext(), TrackingService::class.java).also {
+                it.action = action
+                requireContext().startService(it)
+            }
 
     override fun onStart() {
         super.onStart()
