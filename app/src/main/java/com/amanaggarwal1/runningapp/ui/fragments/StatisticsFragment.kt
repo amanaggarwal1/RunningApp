232d23fb76e0d932616774ec437a34c4eafb2e73
amanaggarwal1 <amanaggarwal2013@gmail.com>
2020-11-10 04:52:14 +0530

    Added data to settings and statistics fragments

--- app/src/main/java/com/amanaggarwal1/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/amanaggarwal1/runningapp/ui/fragments/StatisticsFragment.kt
@@ -1,14 +1,54 @@
 package com.amanaggarwal1.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.amanaggarwal1.runningapp.R
-import com.amanaggarwal1.runningapp.ui.viewmodels.MainViewModel
+import com.amanaggarwal1.runningapp.other.TrackingUtility
 import com.amanaggarwal1.runningapp.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_statistics.*
+import kotlin.math.round
 
 @AndroidEntryPoint
-class StatisticsFragment: Fragment(R.layout.fragment_statistics)  {
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.totalTimeRun.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                tvTotalTime.text = totalTimeRun
+            }
+        })
+        viewModel.totalDistance.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                tvTotalDistance.text = totalDistanceString
+            }
+        })
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                tvAverageSpeed.text = avgSpeedString
+            }
+        })
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val totalCalories = "${it}kcal"
+                tvTotalCalories.text = totalCalories
+            }
+        })
+    }
 }
--- app/src/main/java/com/amanaggarwal1/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/amanaggarwal1/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -1,10 +1,19 @@
 package com.amanaggarwal1.runningapp.ui.viewmodels
 
+
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.amanaggarwal1.runningapp.repository.MainRepository
+import javax.inject.Inject
 
 class StatisticsViewModel @ViewModelInject constructor(
-    val mainRepository: MainRepository
-) : ViewModel() {
+        val mainRepository: MainRepository
+): ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistanceInMeters()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getAverageSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
